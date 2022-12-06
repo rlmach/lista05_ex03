1cfba04c9d01138d055fbce2b3768a0124c99166
Rafael <machiavelli.rafa@gmail.com>
2022-12-06 19:39:03 -0300

    Exercício 3

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    
+    public static void main(String[] args) {
+
+        System.out.println(tresArgumentos(200, 200, 28));
+    }
+
+    public static int tresArgumentos(int a, int b, int c) {
+        int soma = a + b + c;
+        return soma;
+    }
+}
